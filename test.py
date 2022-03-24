f76345e06595ae68cfea67bd4020f847a1fa6fcc
Luca <luca777744@gmail.com>
2022-03-24 17:38:03 +0100

    commit

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ zed = sl.Camera()
 
 # Set configuration parameters
 init_params = sl.InitParameters()
-init_params.camera_resolution = sl.RESOLUTION.HD1080
+init_params.camera_resolution = sl.RESOLUTION.HD720
 init_params.camera_fps = 30
 
 # Open the camera
@@ -17,7 +17,7 @@ if err != sl.ERROR_CODE.SUCCESS:
 codec = cv2.VideoWriter_fourcc('M','P','E','G')
 
 # out = cv2.VideoWriter('./processed.avi', codec, 20.0, (720,1280))
-writer= cv2.VideoWriter('./basicvideo.avi', codec, 20, (1920,1080))
+writer= cv2.VideoWriter('./basicvideo.avi', codec, 20, (1280,720))
 # Create an RGBA sl.Mat object
 image_zed = sl.Mat(zed.get_camera_information().camera_resolution.width, zed.get_camera_information().camera_resolution.height, sl.MAT_TYPE.U8_C4)
 
