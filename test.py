fb53ae19661b28d6518c54968daa4bd48efb7b2f
Luca <luca777744@gmail.com>
2022-03-24 17:43:49 +0100

    commit

--- test.py
+++ test.py
@@ -17,8 +17,8 @@ if err != sl.ERROR_CODE.SUCCESS:
 codec = cv2.VideoWriter_fourcc('M','P','E','G')
 
 # out = cv2.VideoWriter('./processed.avi', codec, 20.0, (720,1280))
-# writer= cv2.VideoWriter('./basicvideo.avi', codec, 20, (1280,720))
-writer= cv2.VideoWriter('./basicvideo.avi', codec, 20, (720,1280))
+writer= cv2.VideoWriter('./basicvideo.avi', codec, 20, (1280,720))
+# writer= cv2.VideoWriter('./basicvideo.avi', codec, 20, (720,1280))
 # Create an RGBA sl.Mat object
 image_zed = sl.Mat(zed.get_camera_information().camera_resolution.width, zed.get_camera_information().camera_resolution.height, sl.MAT_TYPE.U8_C4)
 
@@ -33,8 +33,8 @@ while True:
         zed.retrieve_image(mat, sl.VIEW.LEFT)
         image = mat.get_data()
 
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    image = cv2.resize(image, (720, 1280))
+    # image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    image = cv2.resize(image, (1280, 720))
     writer.write(image)
 
     cv2.imshow('frame', image)
