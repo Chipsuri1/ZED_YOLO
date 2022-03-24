68a00ef119d7bd8506a154a9bec7783125879b65
Luca <luca777744@gmail.com>
2022-03-24 17:33:04 +0100

    commit

--- test.py
+++ test.py
@@ -1,30 +1,36 @@
 import cv2
+import pyzed.sl as sl
 
-cap= cv2.VideoCapture(0)
+# Create a ZED camera object
+zed = sl.Camera()
 
-# def make_720p():
-#     cap.set(3, 1280)
-#     cap.set(4, 720)
-#
-# make_720p()
-width= int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-height= int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+# Set configuration parameters
+init_params = sl.InitParameters()
+init_params.camera_resolution = sl.RESOLUTION.HD1080
+init_params.camera_fps = 30
+
+# Open the camera
+err = zed.open(init_params)
+if err != sl.ERROR_CODE.SUCCESS:
+    exit(-1)
+
 codec = cv2.VideoWriter_fourcc('M','P','E','G')
 
 # out = cv2.VideoWriter('./processed.avi', codec, 20.0, (720,1280))
-writer= cv2.VideoWriter('basicvideo.mp4', codec, 20, (width,height))
-
+writer= cv2.VideoWriter('basicvideo.mp4', codec, 20, (1920,1080))
+# Create an RGBA sl.Mat object
+image_zed = sl.Mat(zed.get_camera_information().camera_resolution.width, zed.get_camera_information().camera_resolution.height, sl.MAT_TYPE.U8_C4)
 
 while True:
-    ret,frame= cap.read()
+    image_ocv = image_zed.get_data()
 
-    writer.write(frame)
+    writer.write(image_ocv)
 
-    cv2.imshow('frame', frame)
+    cv2.imshow('frame', image_ocv)
 
     if cv2.waitKey(1) & 0xFF == 27:
         break
 
-cap.release()
+# cap.release()
 writer.release()
 cv2.destroyAllWindows()
