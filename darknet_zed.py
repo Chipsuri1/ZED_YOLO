56f27c7d0f38706e1155bc2ad449be8926da3dec
Luca <luca777744@gmail.com>
2022-03-24 12:53:14 +0100

    commit

--- darknet_zed.py
+++ darknet_zed.py
@@ -40,48 +40,48 @@ consoleHandler = logging.StreamHandler()
 consoleHandler.setFormatter(logFormatter)
 log.addHandler(consoleHandler)
 
-filename = 'video.avi'
-frames_per_second = 24.0
-res = '720p'
-
-# Set resolution for the video capture
-# Function adapted from https://kirr.co/0l6qmh
-def change_res(cap, width, height):
-    cap.set(3, width)
-    cap.set(4, height)
-
-# Standard Video Dimensions Sizes
-STD_DIMENSIONS =  {
-    "480p": (640, 480),
-    "720p": (1280, 720),
-    "1080p": (1920, 1080),
-    "4k": (3840, 2160),
-}
-
-
-# grab resolution dimensions and set video capture to it.
-def get_dims(cap, res='1080p'):
-    width, height = STD_DIMENSIONS["480p"]
-    if res in STD_DIMENSIONS:
-        width,height = STD_DIMENSIONS[res]
-    ## change the current caputre device
-    ## to the resulting resolution
-    change_res(cap, width, height)
-    return width, height
-
-# Video Encoding, might require additional installs
-# Types of Codes: http://www.fourcc.org/codecs.php
-VIDEO_TYPE = {
-    'avi': cv2.VideoWriter_fourcc(*'XVID'),
-    #'mp4': cv2.VideoWriter_fourcc(*'H264'),
-    'mp4': cv2.VideoWriter_fourcc(*'XVID'),
-}
-
-def get_video_type(filename):
-    filename, ext = os.path.splitext(filename)
-    if ext in VIDEO_TYPE:
-      return  VIDEO_TYPE[ext]
-    return VIDEO_TYPE['avi']
+# filename = 'video.avi'
+# frames_per_second = 24.0
+# res = '720p'
+#
+# # Set resolution for the video capture
+# # Function adapted from https://kirr.co/0l6qmh
+# def change_res(cap, width, height):
+#     cap.set(3, width)
+#     cap.set(4, height)
+#
+# # Standard Video Dimensions Sizes
+# STD_DIMENSIONS =  {
+#     "480p": (640, 480),
+#     "720p": (1280, 720),
+#     "1080p": (1920, 1080),
+#     "4k": (3840, 2160),
+# }
+#
+#
+# # grab resolution dimensions and set video capture to it.
+# def get_dims(cap, res='1080p'):
+#     width, height = STD_DIMENSIONS["480p"]
+#     if res in STD_DIMENSIONS:
+#         width,height = STD_DIMENSIONS[res]
+#     ## change the current caputre device
+#     ## to the resulting resolution
+#     change_res(cap, width, height)
+#     return width, height
+#
+# # Video Encoding, might require additional installs
+# # Types of Codes: http://www.fourcc.org/codecs.php
+# VIDEO_TYPE = {
+#     'avi': cv2.VideoWriter_fourcc(*'XVID'),
+#     #'mp4': cv2.VideoWriter_fourcc(*'H264'),
+#     'mp4': cv2.VideoWriter_fourcc(*'XVID'),
+# }
+#
+# def get_video_type(filename):
+#     filename, ext = os.path.splitext(filename)
+#     if ext in VIDEO_TYPE:
+#       return  VIDEO_TYPE[ext]
+#     return VIDEO_TYPE['avi']
 
 def sample(probs):
     s = sum(probs)
@@ -532,8 +532,8 @@ def main(argv):
     color_array = generate_color(meta_path)
 
     log.info("Running...")
-    cap = cv2.VideoCapture(0)
-    out = cv2.VideoWriter(filename, get_video_type(filename), 25, get_dims(cap, res))
+    # cap = cv2.VideoCapture(0)
+    # out = cv2.VideoWriter(filename, get_video_type(filename), 25, get_dims(cap, res))
 
     key = ''
     while key != 113:  # for 'q' key
@@ -544,7 +544,7 @@ def main(argv):
             zed.retrieve_image(mat, sl.VIEW.LEFT)
             image = mat.get_data()
 
-            out.write(image)
+            # out.write(image)
 
             # get depth information of camera
             zed.retrieve_measure(point_cloud_mat, sl.MEASURE.XYZRGBA)
