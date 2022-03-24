4d7a46cb26718f3622673ed23608e5956ee7133d
Luca <luca777744@gmail.com>
2022-03-24 16:35:31 +0100

    commit

--- test.py
+++ test.py
@@ -1,33 +1,24 @@
-import numpy as np
 import cv2
 
-cap = cv2.VideoCapture(0)
+cap= cv2.VideoCapture(0)
+
+width= int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+height= int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+writer= cv2.VideoWriter('basicvideo.mp4', cv2.VideoWriter_fourcc(*'DIVX'), 20, (width,height))
 
 
-# width = 1280
-# height = 720
-# cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
-# cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
-# Define the codec and create VideoWriter object
-fourcc = cv2.VideoWriter_fourcc('M','P','E','G')
-out = cv2.VideoWriter('output.avi',fourcc, 20.0, (1280,720))
-# out = cv2.VideoWriter('output.avi', -1, 20.0, (640,480))
+while True:
+    ret,frame= cap.read()
 
-while(cap.isOpened()):
-    ret, frame = cap.read()
-    if ret==True:
+    writer.write(frame)
 
-        print(str(frame.shape[0]) + " " + str(frame.shape[1]))
-        # write the flipped frame
-        out.write(frame)
+    cv2.imshow('frame', frame)
 
-        cv2.imshow('frame',frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    else:
+    if cv2.waitKey(1) & 0xFF == 27:
         break
 
-# Release everything if job is finished
+
 cap.release()
-out.release()
+writer.release()
 cv2.destroyAllWindows()
