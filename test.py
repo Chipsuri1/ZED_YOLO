e0051d0eeaff9059b6ec915eed658d7ac4742453
Luca <luca777744@gmail.com>
2022-03-24 17:42:23 +0100

    commit

--- test.py
+++ test.py
@@ -33,8 +33,8 @@ while True:
         zed.retrieve_image(mat, sl.VIEW.LEFT)
         image = mat.get_data()
 
-    cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    image = cv2.resize(image, (720, 1280))
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    # image = cv2.resize(image, (720, 1280))
     writer.write(image)
 
     cv2.imshow('frame', image)
