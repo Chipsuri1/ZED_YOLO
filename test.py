c8eaedb445b99cd055687248e3b248c95487e998
Luca <luca777744@gmail.com>
2022-03-24 16:26:57 +0100

    commit

--- test.py
+++ test.py
@@ -3,9 +3,14 @@ import cv2
 
 cap = cv2.VideoCapture(0)
 
+
+width = 1280
+height = 720
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
 # Define the codec and create VideoWriter object
 fourcc = cv2.VideoWriter_fourcc('M','P','E','G')
-out = cv2.VideoWriter('output.avi',fourcc, 20.0, (4416,1242))
+out = cv2.VideoWriter('output.avi',fourcc, 20.0, (1280,720))
 # out = cv2.VideoWriter('output.avi', -1, 20.0, (640,480))
 
 while(cap.isOpened()):
